cc44cb1071bd6a0bdd4f21746dd6e9926c3f73c8
Isaac Kilis <isaackilis@gmail.com>
2025-06-28 13:21:26 +0100

    Polymorphism example

---
+++ advanced/chapter9/PolymorphismExample.h
@@ -0,0 +1,16 @@
+//
+// Created by Isaac Kilis on 28/6/2025.
+//
+
+#ifndef POLYMORPHISMEXAMPLE_H
+#define POLYMORPHISMEXAMPLE_H
+
+
+
+class PolymorphismExample {
+
+};
+
+
+
+#endif //POLYMORPHISMEXAMPLE_H
